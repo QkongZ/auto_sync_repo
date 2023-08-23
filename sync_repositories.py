bb46c6d5d9fa724e2c6d8cb38c3ff5bb89b75b8c
idoz <96138710+QkongZ@users.noreply.github.com>
2023-08-24 02:39:20 +0800

    Update sync_repositories.py

--- sync_repositories.py
+++ sync_repositories.py
@@ -9,28 +9,28 @@ for repo_info in config['repositories']:
     source_repo = repo_info['source_repo']  
     source_branch = repo_info['source_branch']  
     destination_branch = repo_info['destination_branch']  
-      
+  
     try:  
         # 克隆仓库  
-        subprocess.run(['git', 'clone', source_repo])  
+        subprocess.run(['git', 'clone', source_repo], check=True)  
         print(f"Cloned repository: {source_repo}")  
     except subprocess.CalledProcessError as e:  
         print(f"Failed to clone repository: {source_repo}")  
         print(f"Error message: {e.output}")  
         continue  
-      
+  
     try:  
         # 切换到源分支  
-        subprocess.run(['git', 'checkout', source_branch])  
+        subprocess.run(['git', 'checkout', source_branch], check=True)  
         print(f"Switched to source branch: {source_branch}")  
     except subprocess.CalledProcessError as e:  
         print(f"Failed to switch to source branch: {source_branch}")  
         print(f"Error message: {e.output}")  
         continue  
-      
+  
     try:  
         # 推送分支到目标分支  
-        subprocess.run(['git', 'push', 'origin', f'{source_branch}:{destination_branch}'])  
+        subprocess.run(['git', 'push', 'origin', f'{source_branch}:{destination_branch}'], check=True)  
         print(f"Pushed {source_branch} to {destination_branch}")  
     except subprocess.CalledProcessError as e:  
         print(f"Failed to push {source_branch} to {destination_branch}")  
