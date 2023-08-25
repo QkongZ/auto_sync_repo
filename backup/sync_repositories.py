7b4d3beb5b13829a47d092d70ca0712ad09e56f9
idoz <96138710+QkongZ@users.noreply.github.com>
2023-08-26 02:55:35 +0800

    Add files via upload

---
+++ backup/sync_repositories.py
@@ -0,0 +1,68 @@
+import json
+import os
+from git import Repo
+
+# 从配置文件中读取仓库信息
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+    your_repo_info = config['your_repository']
+
+# 获取GitHub Token（PAT）
+github_token = os.environ['PAT']
+
+for repo_info in config['repositories']:
+    source_repo = repo_info['source_repo']
+    source_branch = repo_info['source_branch']
+    destination_branch = repo_info['destination_branch']
+    excludes = repo_info.get('excludes', [])  # 获取需要排除的文件或文件夹列表，默认为空列表
+
+    try:
+        # 克隆仓库并切换到源分支
+        repo = Repo.clone_from(source_repo, source_repo.split('/')[-1].split('.git')[0])  
+        repo.git.checkout(source_branch)
+        print(f"Cloned repository {source_repo} and switched to source branch {source_branch}")
+    except Exception as e:
+        print(f"Failed to clone repository {source_repo} or switch to source branch {source_branch}")
+        print(f"Error message: {str(e)}")
+        continue
+
+    try:
+        # 设置 Git 配置
+        repo.config_writer().set_value("user", "name", "GitHub Actions").release()
+        repo.config_writer().set_value("user", "email", "actions@github.com").release()
+
+        # 添加目标仓库为远程仓库
+        your_repo_url = f"https://{github_token}@github.com/{your_repo_info['username']}/{your_repo_info['repository']}.git"
+        repo.create_remote('destination', your_repo_url)
+
+        # 拉取目标分支并合并，排除指定的文件或文件夹
+        if destination_branch not in repo.remotes['destination'].refs:
+            # 目标分支不存在，创建它
+            repo.git.branch(destination_branch)
+            repo.remotes['destination'].push(destination_branch)
+            print(f"Created and pushed destination branch {destination_branch}")
+
+        repo.remotes['destination'].fetch(destination_branch)
+        repo.git.checkout(destination_branch)
+        merge_message = f"Merged {source_branch} from {source_repo} into {destination_branch}"
+        for exclude in excludes:
+            if os.path.exists(os.path.join(repo.working_dir, exclude)):
+                if os.path.isdir(exclude):
+                    print(f"Excluding directory: {exclude}")
+                    repo.git.rm('-r', '--cached', exclude)
+            else:
+                 print(f"Excluding file: {exclude}")
+                 repo.git.rm('--cached', exclude)
+        else:
+                print(f"Path does not exist: {exclude}")
+  # 排除指定的文件夹及其子文件或排除文件
+
+        # 提交合并的更改
+        repo.index.commit(merge_message)
+
+        # 推送源分支到目标分支
+        repo.git.push('destination', destination_branch)
+        print(f"Pushed {source_branch} from {source_repo} to {destination_branch} in your repository")
+    except Exception as e:
+        print(f"Error occurred while processing repository {source_repo} and branch {source_branch}")
+        print(f"Error message: {str(e)}")
