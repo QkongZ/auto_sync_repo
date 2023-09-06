7ee8ba6b1c4abd3949e36e3bdde35b0a66a0cb79
idoz <96138710+QkongZ@users.noreply.github.com>
2023-09-07 04:50:28 +0800

    Update sync_repositories.py

--- sync_repositories.py
+++ sync_repositories.py
@@ -3,76 +3,73 @@ import os
 import logging
 from git import Repo
 
-# 设置日志记录
+def log_info(message):
+    logging.info(message)
+
+def log_error(message):
+    logging.error(message)
+
+def clone_and_switch_branch(repo_info):
+    try:
+        repo = Repo.clone_from(repo_info['source_repo'], repo_info['source_repo'].split('/')[-1].split('.git')[0])
+        repo.git.checkout(repo_info['source_branch'])
+        log_info(f"Cloned repository {repo_info['source_repo']} and switched to source branch {repo_info['source_branch']}")
+    except Exception as e:
+        log_error(f"Failed to clone repository {repo_info['source_repo']} or switch to source branch {repo_info['source_branch']}")
+        log_error(f"Error message: {str(e)}")
+        return None
+    return repo
+
+def merge_and_push(repo, repo_info):
+    try:
+        merge_message = f"Merged {repo_info['source_branch']} from {repo_info['source_repo']} into {repo_info['destination_branch']}"
+        if repo_info['excludes']:
+            excludes_string = ', '.join(repo_info['excludes'])
+            merge_message += f" (excluding {excludes_string})"
+        repo.git.merge(f"origin/{repo_info['source_branch']}", message=merge_message)
+        for exclude in repo_info['excludes']:
+            if os.path.exists(os.path.join(repo.working_dir, exclude)):
+                if os.path.isdir(exclude):
+                    log_info(f"Excluding directory: {exclude}")
+                    repo.git.rm('-r', '--cached', exclude)
+                else:
+                    log_info(f"Excluding file: {exclude}")
+                    repo.git.rm('--cached', exclude)
+            else:
+                log_warning(f"Path does not exist: {exclude}")
+        repo.index.commit(merge_message)
+        repo.git.push('destination', repo_info['destination_branch'])
+        log_info(f"Pushed {repo_info['source_branch']} from {repo_info['source_repo']} to {repo_info['destination_branch']} in your repository")
+    except Exception as e:
+        log_error(f"Error occurred while processing repository {repo_info['source_repo']} and branch {repo_info['source_branch']}")
+        log_error(f"Error message: {str(e)}")
+
+# 以下是主程序
 logging.basicConfig(filename='sync_log.txt', level=logging.INFO)
 
-# 从配置文件中读取仓库信息
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
     your_repo_info = config['your_repository']
 
-# 获取GitHub Token（PAT）
 github_token = os.environ['PAT']
 
 for repo_info in config['repositories']:
-    source_repo = repo_info['source_repo']
-    source_branch = repo_info['source_branch']
-    destination_branch = repo_info['destination_branch']
-    excludes = repo_info.get('excludes', [])  # 获取需要排除的文件或文件夹列表，默认为空列表
-
-    try:
-        # 克隆仓库并切换到源分支
-        repo = Repo.clone_from(source_repo, source_repo.split('/')[-1].split('.git')[0])
-        repo.git.checkout(source_branch)
-        logging.info(f"Cloned repository {source_repo} and switched to source branch {source_branch}")
-    except Exception as e:
-        logging.error(f"Failed to clone repository {source_repo} or switch to source branch {source_branch}")
-        logging.error(f"Error message: {str(e)}")
+    repo = clone_and_switch_branch(repo_info)
+    if repo is None:
         continue
 
+    repo.config_writer().set_value("user", "name", "GitHub Actions").release()
+    repo.config_writer().set_value("user", "email", "actions@github.com").release()
+
+    your_repo_url = f"https://{github_token}@github.com/{your_repo_info['username']}/{your_repo_info['repository']}.git"
+    repo.create_remote('destination', your_repo_url)
+
     try:
-        # 设置 Git 配置
-        repo.config_writer().set_value("user", "name", "GitHub Actions").release()
-        repo.config_writer().set_value("user", "email", "actions@github.com").release()
+        repo.remotes['destination'].fetch(repo_info['destination_branch'])
+        repo.git.checkout(repo_info['destination_branch'])
+    except Exception:
+        repo.git.checkout('HEAD', b=repo_info['destination_branch'])
 
-        # 添加目标仓库为远程仓库
-        your_repo_url = f"https://{github_token}@github.com/{your_repo_info['username']}/{your_repo_info['repository']}.git"
-        repo.create_remote('destination', your_repo_url)
+    repo.git.pull('destination', repo_info['destination_branch'])
 
-        # 拉取目标分支的最新代码
-        try:
-            repo.remotes['destination'].fetch(destination_branch)
-            repo.git.checkout(destination_branch)
-        except Exception:
-            # 如果目标分支不存在，创建新的分支
-            repo.git.checkout('HEAD', b=destination_branch)
-
-        repo.git.pull('destination', destination_branch)
-
-        # 合并源分支的代码，排除指定的文件或文件夹
-        merge_message = f"Merged {source_branch} from {source_repo} into {destination_branch}"
-        if excludes:
-            excludes_string = ', '.join(excludes)
-            merge_message += f" (excluding {excludes_string})"
-        repo.git.merge(f"origin/{source_branch}", message=merge_message)
-        for exclude in excludes:
-            if os.path.exists(os.path.join(repo.working_dir, exclude)):
-                if os.path.isdir(exclude):
-                    logging.info(f"Excluding directory: {exclude}")
-                    repo.git.rm('-r', '--cached', exclude)
-                else:
-                    logging.info(f"Excluding file: {exclude}")
-                    repo.git.rm('--cached', exclude)
-            else:
-                logging.warning(f"Path does not exist: {exclude}")
-
-
-        # 提交合并的更改
-        repo.index.commit(merge_message)
-
-        # 推送源分支到目标分支
-        repo.git.push('destination', destination_branch)
-        logging.info(f"Pushed {source_branch} from {source_repo} to {destination_branch} in your repository")
-    except Exception as e:
-        logging.error(f"Error occurred while processing repository {source_repo} and branch {source_branch}")
-        logging.error(f"Error message: {str(e)}")
+    merge_and_push(repo, repo_info)
