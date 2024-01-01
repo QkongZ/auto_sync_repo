dfa828972dc514ac42b268addbf179f8095b68e3
qingshanh <125730412+qingshanh@users.noreply.github.com>
2024-01-01 10:57:32 +0800

    Update app_ydyp.py

--- app_ydyp.py
+++ app_ydyp.py
@@ -738,7 +738,7 @@ class YP:
                 exchange_times = game_info_data.get('result', {}).get('info', {}).get('exchange', 0)
                 print(f'今日剩余游戏次数: {currnum}\n本月排名: {rank}    合成次数: {count}')
                 msg.append(f'今日剩余游戏次数: {currnum}\n本月排名: {rank}    合成次数: {count}')
-                if now.hour > 6 and int(rank) > 94 and exchange_times > 0:
+                if now.hour > 6 and int(rank.replace('+', '')) > 94 and exchange_times > 0:
                     res = self.send_request(f'{exchange_url}?num=1', headers = self.jwtHeaders, cookies = self.cookies)
                     print(res.get('result', {}).get('curr', 0))
                     currnum = res.get('result', {}).get('curr', 0)
